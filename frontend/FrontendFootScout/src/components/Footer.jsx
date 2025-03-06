1daeaabee560212e7325bce3a92e6f356618abc1
Szymi611 <szymondomagala32@gmail.com>
2025-03-06 09:14:00 +0100

    Adding footer

---
+++ frontend/FrontendFootScout/src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import Logo from "../assets/logo.png";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-white rounded-t-lg rounded-tr-lg shadow-sm dark:bg-gray-900 mt-4">
+        <div className="w-full max-w-screen-xl mx-auto p-2 md:py-4">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="#" className="flex items-center space-x-3 text-white">
+              <img src={Logo} className="h-17 bg-gray-900" alt="Logo app" />
+              <span
+                className="relative self-center text-2xl font-semibold whitespace-nowrap
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-700 after:transition-all after:duration-300 hover:after:w-full"
+              >
+                FootRadar
+              </span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" className="me-4 md:me-6 relative self-center text-md font-semibold whitespace-nowrap
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-700 after:transition-all after:duration-300 hover:after:w-full  ">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="me-4 md:me-6 relative self-center text-md font-semibold whitespace-nowrap
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-700 after:transition-all after:duration-300 hover:after:w-full ">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="me-4 md:me-6 relative self-center text-md font-semibold whitespace-nowrap
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-700 after:transition-all after:duration-300 hover:after:w-full  ">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="relative self-center text-md font-semibold whitespace-nowrap
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-700 after:transition-all after:duration-300 hover:after:w-full "
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-4" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2025{" "}
+            <a href="/" className="hover:underline">
+              FootRadar
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+}
--- frontend/FrontendFootScout/src/components/MainPage.jsx
+++ frontend/FrontendFootScout/src/components/MainPage.jsx
@@ -1,4 +1,5 @@
 import stadium from "../assets/stadium.jpeg";
+import Footer from "./Footer";
 import ArticleBP from "./MPComponents/ArticleBP";
 import Badges from "./MPComponents/Badges";
 import LeagueTable from "./MPComponents/LeagueTable";
@@ -12,14 +13,13 @@ export default function MainPage() {
   return (
     <>
       <Navbar />
-      <div className="relative w-full h-400">
-        <div className="relative w-full h-1/3">
+      <div className="relative w-full ">
+        <div className="relative w-full">
           <img
             src={stadium}
             className="w-full h-full object-cover"
             alt="Stadion"
           />
-
           <div className="absolute bottom-0 left-0 w-full h-86 bg-gradient-to-t from-black/90 to-transparent"></div>
 
           <div className="absolute bottom-2 left-2 text-white px-3 py-1 ">
@@ -27,26 +27,12 @@ export default function MainPage() {
           </div>
         </div>
 
-        {/* <div className="flex flex-col gap-2">
-          <Badges />
-          <div className="grid grid-cols-3 grid-rows-2 gap-2 h-[500px] w-full">
-            <div className="row-span-2 rounded-2xl bg-transparent ">
-              <ShortenTables />
-            </div>
-            <div className="row-span-2 bg-sky-500">
-              <ScorersShorten/>
-            </div>
-            <div className="row-span-2 bg-fuchsia-700">
-              <NextGames />
-            </div>
-          </div>
-        </div> */}
-
         <div className="flex flex-col gap-2">
           <Badges />
           <Shorten />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
